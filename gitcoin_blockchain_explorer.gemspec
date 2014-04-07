b282d607d8ef8c901602eb71a1c283279f2dd0cc
Carlos Cardona <cgcardona@gmail.com>
2014-04-06 17:44:35 -1000

    Pushing gem to github to test in my actual app.

--- gitcoin_blockchain_explorer.gemspec
+++ gitcoin_blockchain_explorer.gemspec
@@ -1,12 +1,13 @@
 Gem::Specification.new do |gem|
-  gem.name        = 'gitcoin_blockchain_explorer'
-  gem.version     = '0.1.1'
-  gem.date        = Date.today.to_s
-  gem.summary     = "Gitcoin.co's Blockchain Explorer"
-  gem.description = "Gitcoin.co's Blockchain Explorer"
-  gem.authors     = ["Carlos Cardona"]
-  gem.email       = 'gitcoin.co@gmail.com'
-  gem.files       = ["lib/gitcoin_blockchain_explorer.rb"]
-  gem.homepage    = 'http://rubygems.org/gems/gitcoin_blockchain_explorer'
-  gem.license     = 'MIT'
+  gem.name           = 'gitcoin_blockchain_explorer'
+  gem.version        = '0.1.1'
+  gem.date           = Date.today.to_s
+  gem.summary        = "Gitcoin.co's Blockchain Explorer"
+  gem.description    = "Gitcoin.co's Blockchain Explorer"
+  gem.authors        = ["Carlos Cardona"]
+  gem.email          = 'gitcoin.co@gmail.com'
+  gem.add_dependency "httparty", ["~> 0.13.0"]
+  gem.files          = ["lib/gitcoin_blockchain_explorer.rb"]
+  gem.homepage       = 'http://rubygems.org/gems/gitcoin_blockchain_explorer'
+  gem.license        = 'MIT'
 end
--- lib/gitcoin_blockchain_explorer.rb
+++ lib/gitcoin_blockchain_explorer.rb
@@ -1,3 +1,5 @@
+'reauire httparty'
+
 module GitcoinBlockchainExplorer
   class Explorer
     attr_accessor :coinbase_api_base, :blockr_api_base, :blockchain_api_base, :cex_api_base
@@ -11,27 +13,36 @@ module GitcoinBlockchainExplorer
 
 
     def last_block()
-      puts 'last_block called'
+      HTTParty.get("#{self.blockr_api_base}coin/info")['data']
     end
 
-    def last_blocks(number_of_blocks)
-      puts "last_blocks called with #{number_of_blocks}"
+    def last_blocks(number_of_blocks = 5)
+      #HTTParty.get("#{Figaro.env.blockr_api_base}block/info/#{last_block_num},#{last_block_num - 1},#{last_block_num - 2},#{last_block_num - 3},#{last_block_num - 4},#{last_block_num - 5}")['data']
+      HTTParty.get("#{self.blockr_api_base}block/info/#{HTTParty.get("#{self.blockr_api_base}coin/info")['data']['last_block']['nb']}")['data']
     end
 
-    def last_transactions(number_of_transactions)
-      puts "last_transactions called with #{number_of_transactions}"
+    def last_transactions(number_of_transactions = 10)
+      HTTParty.get("#{self.blockr_api_base}block/txs/last")
     end
 
     def block(block_number)
-      puts "block called with #{block_number}"
+      HTTParty.get("#{self.blockr_api_base}block/info/#{block_number}")
     end
 
     def block_transactions(block_number)
-      puts "block_transactions called with #{block_number}"
+      HTTParty.get("#{self.blockr_api_base}block/txs/#{block_number}")
+    end
+
+    def transaction(transaction_hash)
+      HTTParty.get("#{self.blockr_api_base}tx/info/#{transaction_hash}")
     end
         
     def address(public_address)
-      puts "address called with #{public_address}"
+      HTTParty.get("#{Figaro.env.blockr_api_base}address/info/#{public_address}")
+    end
+        
+    def address_transactions(public_address)
+      HTTParty.get("#{Figaro.env.blockr_api_base}address/txs/#{public_address}")
     end
   end
 end
