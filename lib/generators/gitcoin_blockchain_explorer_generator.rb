e3a10c931648172c671e44542b2a7796b7728c45
Carlos Cardona <cgcardona@gmail.com>
2014-04-13 07:29:37 -1000

    Update path and add puts.

--- lib/generators/gitcoin_blockchain_explorer_generator.rb
+++ lib/generators/gitcoin_blockchain_explorer_generator.rb
@@ -3,19 +3,30 @@ class GitcoinBlockchainExplorerGenerator < Rails::Generators::NamedBase
 
   def copy_files
     puts 'inject routes'
-    prepend_file 'config/environments/test.rb' do
+    prepend_file 'config/routes.rb' do
+      puts 'index route'
       "\n get 'blockchain', to: 'blockchain#index'\n"
+
+      puts 'search route'
       "\n post 'blockchain/search, to: 'blockchain#search'\n"
+
+      puts 'block route'
       "\n get 'blockchain/block', to 'blockchain#block'\n"
+
+      puts 'transaction route'
       "\n get 'blockchain/transaction', to 'blockchain#transaction'\n"
+
+      puts 'address route'
       "\n get 'blockchain/address', to 'blockchain#address'\n"
     end
 
     puts 'Create controller and views'
     ['blockchain_controller', 'index', 'search', 'block', 'transaction'].each_with_index do |page, index|
       if index == 0
+        puts 'blockchain controller'
         copy_file 'blockchain_controller.rb', 'app/controllers/blockchain_controller.rb'
       else
+        puts "#{page} view"
         copy_file "#{page}.html.haml", "app/views/blockchain_controller/#{page}.html.haml"
       end
     end
